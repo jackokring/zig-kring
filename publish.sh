bad89cee830b4437fb13b5e4ee79a755bf9cec47
jackokring <jackokring@gmail.com>
2023-10-01 15:56:51 +0000

    expected error

--- publish.sh
+++ publish.sh
@@ -4,7 +4,10 @@ zig build
 zig build
 cp zig-out/lib/libkring.so zig-out/kring/kring.so
 cd zig-out
-./publish.sh
+# process python publish
+./publish.sh || echo "Problem publishing python module. Version incremented?"
 cd ../..
 zig build
+# run zig tests
+zig build test
 
